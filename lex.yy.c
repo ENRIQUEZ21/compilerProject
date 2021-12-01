0927ad62050b0dbbd0a232ef39778e360b962d8d
ENRIQUEZ21 <enriquezgabriel426@gmail.com>
2021-12-01 20:18:36 +0100

    Add first compilation of one 0

--- lex.yy.c
+++ lex.yy.c
@@ -282,11 +282,11 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
-static yyconst short int yy_accept[8] =
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
+static yyconst short int yy_accept[10] =
     {   0,
-        0,    0,    4,    2,    3,    1,    0
+        0,    0,    6,    4,    5,    1,    2,    3,    0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -295,16 +295,16 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    3,    3,    1,    3,    1,    3,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    3,    3,    1,    3,    1,    3,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    1,    1,    1,
+        1,    1,    1,    1,    5,    5,    5,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        1,    1,    1,    1,    1,    1,    5,    5,    5,    5,
 
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        5,    5,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -321,29 +321,31 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[4] =
+static yyconst int yy_meta[6] =
     {   0,
-        1,    1,    1
+        1,    1,    1,    1,    1
     } ;
 
-static yyconst short int yy_base[8] =
+static yyconst short int yy_base[10] =
     {   0,
-        0,    0,    4,    5,    5,    5,    5
+        0,    0,    6,    7,    7,    7,    7,    7,    7
     } ;
 
-static yyconst short int yy_def[8] =
+static yyconst short int yy_def[10] =
     {   0,
-        7,    1,    7,    7,    7,    7,    0
+        9,    1,    9,    9,    9,    9,    9,    9,    0
     } ;
 
-static yyconst short int yy_nxt[9] =
+static yyconst short int yy_nxt[13] =
     {   0,
-        4,    5,    6,    7,    3,    7,    7,    7
+        4,    5,    6,    7,    8,    9,    3,    9,    9,    9,
+        9,    9
     } ;
 
-static yyconst short int yy_chk[9] =
+static yyconst short int yy_chk[13] =
     {   0,
-        1,    1,    1,    3,    7,    7,    7,    7
+        1,    1,    1,    1,    1,    3,    9,    9,    9,    9,
+        9,    9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -360,8 +362,8 @@ char *yytext;
 #line 1 "Compiler.l"
 #define INITIAL 0
 #line 2 "Compiler.l"
-    #include "stdio.h"
-#line 365 "lex.yy.c"
+    #include <stdio.h>
+#line 367 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -515,7 +517,7 @@ YY_DECL
 #line 5 "Compiler.l"
 
 
-#line 519 "lex.yy.c"
+#line 521 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -566,13 +568,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 8 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 5 );
+		while ( yy_base[yy_current_state] != 7 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -601,19 +603,29 @@ do_action:	/* This label is used only to access EOF actions. */
 case 1:
 YY_RULE_SETUP
 #line 7 "Compiler.l"
-return ARITHMETIC_OPERATOR;
+printf("ARITHMETIC_OPERATOR");
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
 #line 8 "Compiler.l"
-printf("Unexpected character")
+printf("DIGIT");
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
+#line 9 "Compiler.l"
+printf("LETTER");
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
 #line 10 "Compiler.l"
+printf("Unexpected character");
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 12 "Compiler.l"
 ECHO;
 	YY_BREAK
-#line 617 "lex.yy.c"
+#line 629 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -905,7 +917,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 8 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -940,11 +952,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 8 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 7);
+	yy_is_jam = (yy_current_state == 9);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1499,5 +1511,14 @@ int main()
 	return 0;
 	}
 #endif
-#line 10 "Compiler.l"
+#line 12 "Compiler.l"
+
+
+int yywrap(void) {return 1;}
+int main() {
+    yyin = fopen("input.txt", "r");
+    yyout = fopen("output.txt", "w");
+
+    fprintf(yyout, "These are the tokens");
+}
 
